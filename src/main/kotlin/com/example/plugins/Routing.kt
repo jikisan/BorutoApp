6eb90de0180e2559ee48fe594b2e1d69db143d19
Kyle <jsanterna@timex.com>
2024-02-25 22:17:33 +0800

    Change foler name Image to image

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,14 +1,11 @@
 package com.example.plugins
 
 import com.example.routes.getAllHeroes
-import com.example.routes.getAllHeroesAlternative
 import com.example.routes.root
 import com.example.routes.searchHeroes
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import javax.naming.AuthenticationException
 
 fun Application.configureRouting() {
     routing {
@@ -17,15 +14,10 @@ fun Application.configureRouting() {
         getAllHeroes()
         searchHeroes()
 
-//        get("/test2"){
-//            throw AuthenticationException()
-//
+        staticResources("/images", "images")
+
+//        static("/images") {
+//            resources("images")
 //        }
-
-//        staticResources("/images", "images")
-
-        static("/images") {
-            resources("images")
-        }
     }
 }
